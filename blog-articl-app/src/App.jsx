a897c25a8827571c58ce8bac9f4438d437d94f18
AYOUB-EL <52068209+bbouya@users.noreply.github.com>
2022-03-25 23:39:53 +0100

    Update App.jsx

--- blog-articl-app/src/App.jsx
+++ blog-articl-app/src/App.jsx
@@ -5,8 +5,6 @@ import Blog from './pages/Blog'
 import Home from './pages/Home';
 
 import {BrowserRouter as Router, Route, Switch, IndexRoute, Link} from 'react-router-dom';
-
-
 function App() {
   const Homes = () => <h1>ana ayoub</h1>
   const About = () => <h1>About Us</h1>
