5f9d1ed8cabeadfe4be040056d30f18c2aea44d5
AYOUB-EL <52068209+bbouya@users.noreply.github.com>
2022-03-26 23:36:09 +0100

    add

---
+++ blog-articl-app/src/pages/components/Home/BlogList/BlogItem/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './styles.css'
+
+const BlogItem = () => {
+  return (
+    <div>BlogItem</div>
+  )
+}
+
+export default BlogItem
---
+++ blog-articl-app/src/pages/components/Home/BlogList/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './styles.css'
+
+const Blogist = () => {
+  return (
+    <div className='blogList-wrap'> 
+        {Blogist.map(blog=>)}
+    </div>
+  )
+}
+
+export default Blogist
